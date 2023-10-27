72b442eaf8fac87f1b59aa05ebb97cf85855e952
Rowland <rowland.wanderi@student.moringaschool.com>
2023-10-27 12:04:04 +0300

    added validating errors' git push

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,17 +12,25 @@ function Form() {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+  //Add state for holding error messages
+  const[error, setErrors] = useState([])
 function handleSubmit(event){
-            event.preventDefault()
-            const formData ={
-              firstName:firstName,
-              lastName:lastName,
-            }
-            const dataArray = [...submittedData,formData]
-            setSubmittedData(dataArray)
-            //clears out the input field after submitting the form
-            setFirstName("")
-            setLastName("")
+              event.preventDefault()
+              //the first name is always required so use an if statement to check its presence
+              if(firstName.length>0){
+              const formData ={
+                firstName:firstName,
+                lastName:lastName,
+              }
+                const dataArray = [...submittedData,formData]
+                setSubmittedData(dataArray)
+                //clears out the input field after submitting the form
+                setFirstName("")
+                setLastName("")
+                setErrors([])
+              }else{
+                setErrors(["First name is required"])
+              }
             }
           const listOfSubmissions = submittedData.map((data, index)=>{
             return (
@@ -38,6 +46,8 @@ function handleSubmit(event){
           <input type="text" onChange={handleLastNameChange} value={lastName} />
           <button type="submit">Submit</button>
         </form>
+        {/*this is where we conditionally show the user the errors */}
+        {error.lenght > 0 ? error.map((error,index) => (<p key ={index} style={{color:"red"}}>{error}</p>)) : null }
         <h1>LIST OF SUBMISSIONS</h1>
         {listOfSubmissions}
     </div>
